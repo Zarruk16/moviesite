d3d57c38d966f7d4d8aaec579a58433f33a5c60e
Abdulsalam zarruk <120612583+Zarruk16@users.noreply.github.com>
2023-09-27 01:41:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import searchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com?apikey=455bdc48";
 
-// const movie1 = {
-//   Title: "Shrek",
-//   Year: "2001",
-//   imdbID: "tt0126029",
-//   Type: "movie",
-//   Poster:
-//     "https://m.media-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-// };
+const movie1 = {
+  Title: "Shrek",
+  Year: "2001",
+  imdbID: "tt0126029",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+};
 
 const App = () => {
   const [movies, setMovies] = useState([]);
